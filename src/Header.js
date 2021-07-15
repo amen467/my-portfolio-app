52afe831373f0bddfc2621b1da91dd0023448d0e
amen467 <amen467@gmail.com>
2021-07-15 19:23:13 -0400

    A bunch of stuff

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header-left">
+        <img
+          src="https://cdn.pixabay.com/photo/2016/03/18/22/33/camera-1265913_960_720.png"
+          alt="" />
+          <h1>Your Name</h1>            
+      </div>
+
+      <div className="header-right">
+        
+            
+      </div>
+            
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Slideshow.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Slideshow.css';
+// import emma from "./images/scale_2400.jpg";
+// import watson from "./images/watson.jpg";
+import anya from "./images/anya-camera.jpg";
+
+function slideshow() {
+  return (
+    <div className="slideshow">
+      {/* <img src={emma} alt="Emma Roberts with Money"></img> */}
+      {/* <img src={watson} alt="Emma Watson"></img> */}
+      <img src={anya} alt="Anya with a camera"></img>
+    </div>
+  )
+}
+
+export default slideshow
